6e16533e052e05034f8ee1104bcb24977618d443
Luka Furlan <luka@evriala.net>
2014-10-15 14:54:00 +0100

    Ability to send metrics to CloudWatch.

---
+++ aws.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"time"
+
+	"github.com/crowdmob/goamz/aws"
+	"github.com/crowdmob/goamz/cloudwatch"
+)
+
+// The CW type encapsulates cloudwatch.CloudWatch to provide additional method(s).
+type CW struct {
+	*cloudwatch.CloudWatch
+}
+
+// NewCW creates a new aws.Auth using credentials located by aws.GetAuth and returns a CW struct.
+func NewCW(accessKey, secretKey, region string) (*CW, error) {
+	now := time.Now()
+
+	auth, err := aws.GetAuth(accessKey, secretKey, "", now)
+	if err != nil {
+		return nil, err
+	}
+
+	cw, err := cloudwatch.NewCloudWatch(auth, aws.Regions[region].CloudWatchServicepoint)
+	return &CW{cw}, err
+}
+
+// PutMetrics saves provided information to CloudWatch and returns aws.BaseResponse.
+func (cw *CW) PutMetrics(value uint64, unit string, name string, namespace string, autoScalingGroup string) (*aws.BaseResponse, error) {
+	instanceId := aws.InstanceId()
+
+	dimensions := []cloudwatch.Dimension{
+		{Name: "InstanceID", Value: instanceId},
+	}
+
+	if autoScalingGroup != "" {
+		dimensions = append(
+			dimensions,
+			cloudwatch.Dimension{
+				Name: "AutoScalingGroupName", Value: autoScalingGroup,
+			},
+		)
+	}
+
+	metric := cloudwatch.MetricDatum{
+		Dimensions: []cloudwatch.Dimension{
+			{Name: "InstanceID", Value: instanceId},
+		},
+		MetricName: name,
+		Unit:       unit,
+		Value:      float64(value),
+	}
+
+	return cw.PutMetricDataNamespace([]cloudwatch.MetricDatum{metric}, namespace)
+}
---
+++ aws_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+const (
+	fakeAccessKey = "fakeId"
+	fakeSecretKey = "fakeSecret"
+	fakeRegion    = "eu-west-1"
+)
+
+func TestNewCW(t *testing.T) {
+	if err := os.Setenv("AWS_ACCESS_KEY_ID", fakeAccessKey); err != nil {
+		t.Errorf("Unable to set AWS_ACCESS_KEY_ID environment variable.")
+	}
+
+	if err := os.Setenv("AWS_SECRET_ACCESS_KEY", fakeSecretKey); err != nil {
+		t.Errorf("Unable to set AWS_SECRET_ACCESS_KEY environment variable.")
+	}
+
+	// Fetch credentials from the environment variables created above.
+	var cw *CW
+	var err error
+	cw, err = NewCW("", "", fakeRegion)
+	if err != nil {
+		t.Errorf("Failed to create a new CW struct: %s", err.Error())
+	}
+
+	t.Logf("Return value of NewCW: %#v", cw)
+}
